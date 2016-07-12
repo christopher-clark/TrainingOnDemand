ec3e506f51fd734ba5c0db6e7645cd88c28c8d3e
christopher-clark <g.kanizaj.clark@gmail.com>
2016-07-12 17:58:01 +1000

    Initial Commit of Lesson 1 code

---
+++ Training On Demand/src/com/tod/oracle/NewNumbers.java
@@ -0,0 +1,13 @@
+package com.tod.oracle;
+
+public class NewNumbers {
+
+	private static long new_long = 1234_4567_89;
+	private static int int1 = 0b0011_0011_0011_0011_0011_0011_0011_0011;
+	
+	public static void main(String[] args) {
+		System.out.println("New long 1234_4567_89 = " + new_long);
+		System.out.println("int1 = 0b0011_0011_0011_0011 " + int1);
+	}
+
+}
---
+++ Training On Demand/src/com/tod/oracle/Strings.java
@@ -0,0 +1,14 @@
+package com.tod.oracle;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		char character = '!';
+		String string1 = "Hello";
+		String string2 = "World";
+		String string3 = string1 + " " + string2;
+		System.out.println(string3 + character);
+
+	}
+
+}
